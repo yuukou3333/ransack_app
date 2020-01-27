b11a6b3ca2585d997c285f4ecb50a32ebefc6db5
yuukou3333 <kuusou3333@gmail.com>
2020-01-27 12:52:15 +0900

    ransackを使用したアプリ作成

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: '斉藤', age: 35)
+User.create(name: '田中', age: 21)
+User.create(name: '佐藤', age: 32)
+User.create(name: '山岸', age: 33)
+User.create(name: '平', age: 26)
+User.create(name: '加藤', age: 25)
